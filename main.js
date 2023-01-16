9e4f2e8d4004a4744ed09bb761c271da3f59ca36
azaelreyes <azaelreyes20.ar@gmail.com>
2023-01-16 15:51:41 -0800

    added a form to create new tasks

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-console.log("This works");
+(()=>{const e=document.getElementById("addCircle"),t=document.getElementById("mainAddTask"),s=document.getElementById("blurBackground"),l=document.getElementById("exitAddTask");e.addEventListener("click",(()=>{t.classList.toggle("hidden"),t.classList.toggle("visible"),s.classList.toggle("hidden"),s.classList.toggle("visible")})),l.addEventListener("click",(()=>{t.classList.toggle("hidden"),t.classList.toggle("visible"),s.classList.toggle("hidden"),s.classList.toggle("visible")}))})();
